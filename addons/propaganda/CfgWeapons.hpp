741d17e7b8e7e92a4a546de422f39d8945c7d612
OverlordZorn <dennisAzorn@gmail.com>
2026-08-18 14:27:05 +0200

    Create CfgWeapons.hpp

---
+++ addons/propaganda/CfgWeapons.hpp
@@ -0,0 +1,36 @@
+class CfgWeapons {
+
+    class CBA_MiscItem_ItemInfo;
+
+    class ACE_SpraypaintBlack;
+    class ACE_SpraypaintWhite;
+    class ACE_SpraypaintRed;
+
+    class CVO_SpraypaintBlack: ACE_SpraypaintBlack {
+        author = "Zorn";
+        displayName = "CVO Spraycan";
+        descriptionShort = "A can of spray paint for tagging walls with glorious propaganda.";
+        scope = 1;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
+    class CVO_SpraypaintWhite: ACE_SpraypaintWhite {
+        author = "Zorn";
+        displayName = "CVO Spraycan";
+        descriptionShort = "A can of spray paint for tagging walls with glorious propaganda.";
+        scope = 1;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
+    class CVO_SpraypaintRed: ACE_SpraypaintRed {
+        author = "Zorn";
+        displayName = "CVO Spraycan";
+        descriptionShort = "A can of spray paint for tagging walls with glorious propaganda.";
+        scope = 1;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
+};
